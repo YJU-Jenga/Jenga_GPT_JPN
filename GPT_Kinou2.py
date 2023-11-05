75812a074ec2aafb67d01f5546427ca815773126
Donggeun <ezfood3@gmail.com>
2023-11-06 03:10:52 +0900

    update: GPT_JP

--- GPT_Kinou2.py
+++ GPT_Kinou2.py
@@ -116,7 +116,7 @@ def play_fairy_tale(database_list):
 
         if matching_tale:
             text_to_speech(text + "童話を聞かせてあげるよ。")
-            pygame.mixer.Sound("momotarou.mp3").play()
+            pygame.mixer.Sound("momotarou.wav").play()
         else:
             text_to_speech("そんな童話はない。")
 
