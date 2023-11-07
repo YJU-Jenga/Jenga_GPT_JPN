14152ac743a7729e8361c699a8a072ee2f2d2d92
Donggeun <ezfood3@gmail.com>
2023-11-07 23:16:18 +0900

    update: db

---
+++ db.py
@@ -0,0 +1,33 @@
+import pymysql
+import config
+
+
+def connect_to_database():
+    db = pymysql.Connect(
+        host='localhost',
+        user='jenga',
+        password=config.database_password,
+        database='jenga',
+        charset='utf8',
+    )
+    return db
+
+
+db = pymysql.Connect(
+    host='localhost',
+    user='jenga',
+    password=config.database_password,
+    database='jenga',
+    charset='utf8',
+)
+
+cursor = db.cursor()
+create_table_query = """
+CREATE TABLE book (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    detail TEXT
+)
+"""
+
+cursor.execute(create_table_query)
