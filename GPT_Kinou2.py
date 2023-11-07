4a339652c43650e31ebad9d6b8b1f3078e7f8a0b
Donggeun <ezfood3@gmail.com>
2023-11-07 21:22:17 +0900

    update: GPT_JP

--- GPT_Kinou2.py
+++ GPT_Kinou2.py
@@ -149,14 +149,10 @@ def main():
                     play_fairy_tale(database_list)
                 else:
                     print("GPT")
-                    response = openai.Completion.create(
+                    response = openai.ChatCompletion.create(
                         model="gpt-3.5-turbo",
-                        prompt=text,
-                        temperature=0.9,
-                        max_tokens=2048,
-                        top_p=1,
-                        frequency_penalty=0.0,
-                        presence_penalty=0.6,
+                        messages=[{"role": "system", "content": "You are a helpful assistant."},
+                                  {"role": "user", "content": text}],
                     )
                     message = response.choices[0].text.strip()
                     print("response: ", response)
